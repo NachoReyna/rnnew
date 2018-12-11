7167eb1816d0021d96bc599cc61a07ac20473ad4
Ignacio Reyna <ignacio.reyna@reprisedigital.com>
2018-12-10 18:53:12 -0600

    inicio dos

---
+++ assets/js/generals.js
@@ -0,0 +1,15 @@
+/**
+ * IPG Mediabrands
+ *
+ *
+ * This file contains IPG Mediabrands Information
+ * Use, disclosure or reproduction is prohibited.
+ *
+ * @author    IPG Mediabrands / Benjamín García
+
+ */
+
+
+$( document ).ready(function() {
+	//Start the Party
+});
