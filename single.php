e89d000c3382beaa77bcdabe2c3b2a909ef704c0
Ignacio Reyna <ignacio.reyna@reprisedigital.com>
2018-12-27 16:50:31 -0600

    [RN] - detalles finales

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
       if ( have_posts() ) { 	while ( have_posts() ) { the_post();
         $video = get_field('video');
         $titulo = get_the_title();
-        $contenido = get_the_content();
+        $author = get_the_author();
         ?>
         <div class="container">
           <div class="row" align="center">
@@ -15,25 +15,36 @@
   <div class="row no-gutters">
   <!--Contenido del post-->
     <div class="col-12 col-sm-12 col-md-9">
-      <h2 align="center"><?php echo $titulo; ?></h2>
+      <h1 align="center"><?php echo $titulo; ?></h1>
+
       <div class="contenedor-texto">
-        <?php echo $contenido ?>
+        <div class="autor-nota">
+          <p>Por <?php echo $author; ?> <span>|</span>  <?php echo the_date(); ?> </p>
+        </div>
+        <?php the_content(); ?>
       </div>
-      <div class="embed-responsive embed-responsive-16by9" align="center">
-        <iframe class="embed-responsive-item" src="<?php echo $video; ?>" allowfullscreen></iframe>
-      </div>
+      <?php if($video){?>
+        <div class="embed-responsive embed-responsive-16by9" align="center">
+          <iframe class="embed-responsive-item" src="<?php echo $video; ?>" allowfullscreen></iframe>
+        </div>
+        <?php } ?>
+        <?php if($sporify){?>
+          <div class="embed-responsive embed-responsive-16by9" align="center">
+            <iframe class="embed-responsive-item" src="<?php echo $video; ?>" allowfullscreen></iframe>
+          </div>
+          <?php } ?>
       <?php }//endwhile
       }//endif ?>
     </div>
     <!--Contenido extra como secciones de redes sociales etc-->
     <div class="col-12 col-sm-12 col-md-3" align="center">
       <div class="col-md-12 col-sm-12 col-lg-12">
-        <iframe src="https://open.spotify.com/embed/user/bye83q42d2u9vooxycaswwd2w/playlist/4dA8OfAU1nnLJUPeXWSSKI" class="spotify-elemento"  frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe src="https://open.spotify.com/user/bye83q42d2u9vooxycaswwd2w/playlist/4dA8OfAU1nnLJUPeXWSSKI?si=CZ2MexqwTE2psKQKcjuaoA" class="spotify-elemento"  frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
       <div class="col-md-12 col-sm-12 col-lg-12">
         <h4 class="noticias-letrero">Más Noticias</h4>
       </div>
-        <?php $the_query = new WP_Query( array( 'category_name' => 'noticias', 'posts_per_page' => 6 ) );?>
+        <?php $the_query = new WP_Query( array( 'category_name' => 'noticias', 'posts_per_page' => 3 ) );?>
         <?php if ( $the_query->have_posts() ) : ?>
         <?php while ( $the_query->have_posts() ) : $the_query->the_post();?>
           <?php $author = get_the_author(); ?>
