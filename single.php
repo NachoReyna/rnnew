e66a9895065e80cd1b4caeac4b0c30aa27e343c5
Ignacio Reyna <ignacio.reyna@reprisedigital.com>
2018-12-27 13:22:41 -0600

    [RN] - Spotify

--- single.php
+++ single.php
@@ -12,23 +12,24 @@
               <img src="<?php echo get_the_post_thumbnail_url(); ?> " alt="" width="100%">
             </div>
           </div>
-          <div class="row no-gutters">
-            <!--Contenido del post-->
-          <div class="col-12 col-sm-12 col-md-9">
-          <h2 align="center">
-            <?php echo $titulo; ?>
-          </h2>
-          <div class="contenedor-texto">
-            <?php echo $contenido ?>
-          </div>
-        <div class="embed-responsive embed-responsive-16by9" align="center">
-          <iframe class="embed-responsive-item" src="<?php echo $video; ?>" allowfullscreen></iframe>
-        </div>
-      	<?php }//endwhile
+  <div class="row no-gutters">
+  <!--Contenido del post-->
+    <div class="col-12 col-sm-12 col-md-9">
+      <h2 align="center"><?php echo $titulo; ?></h2>
+      <div class="contenedor-texto">
+        <?php echo $contenido ?>
+      </div>
+      <div class="embed-responsive embed-responsive-16by9" align="center">
+        <iframe class="embed-responsive-item" src="<?php echo $video; ?>" allowfullscreen></iframe>
+      </div>
+      <?php }//endwhile
       }//endif ?>
     </div>
     <!--Contenido extra como secciones de redes sociales etc-->
     <div class="col-12 col-sm-12 col-md-3" align="center">
+      <div class="col-md-12 col-sm-12 col-lg-12">
+        <iframe src="https://open.spotify.com/embed/user/bye83q42d2u9vooxycaswwd2w/playlist/4dA8OfAU1nnLJUPeXWSSKI" class="spotify-elemento"  frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
       <div class="col-md-12 col-sm-12 col-lg-12">
         <h4 class="noticias-letrero">Más Noticias</h4>
       </div>
@@ -37,29 +38,23 @@
         <?php while ( $the_query->have_posts() ) : $the_query->the_post();?>
           <?php $author = get_the_author(); ?>
           <div class="col-md-12 col-sm-12 col-lg-12">
-            <div class="card mb-4 box-shadow estilo-noticias" style="background-image:url('<?php the_post_thumbnail_url(); ?>')"></div>
-            <div class="card-body">
+            <div class="card box-shadow estilo-noticias" style="background-image:url('<?php the_post_thumbnail_url(); ?>')"></div>
+            <div class="card-body card-text">
               <a class="text-dark" href="<?php the_permalink(); ?>"> <?php the_title(); ?></a>
               <p><?php echo $author;?> <br> <?php echo the_date(); ?>  </p>
             </div>
-
           </div>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
         <?php else : ?>
         <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
         <?php endif; ?>
-
-      <div class="col-md-12 col-sm-12 col-lg-12">
-        <h4 class="noticias-letrero instagram">Instagram</h4>
-        <?php echo do_shortcode( '[insta-gallery id="1"]' ); ?>
-    </div>
-
-
-          </div>
+        <div class="col-md-12 col-sm-12 col-lg-12">
+          <h4 class="noticias-letrero instagram">Instagram</h4>
+          <?php echo do_shortcode( '[insta-gallery id="1"]' ); ?>
         </div>
+      </div>
+    </div>
   </div>
 </div>
-
-
 <?php get_footer();?>
